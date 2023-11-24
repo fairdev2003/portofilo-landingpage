8f60517d983c82db469809d52873700a974b7aab
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2023-11-24 13:46:07 +0100

    Added github logo to skills section

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -9,6 +9,7 @@ import TailwindCSS from '../assets/tailwindcss.png'
 import ReactIcon from '../assets/react.png'
 import SQL from '../assets/mysql.png'
 import NodeJS from '../assets/nodejs.png'
+import GIT from '../assets/git.png'
 
 export default function Skills() {
   return (
@@ -79,6 +80,13 @@ export default function Skills() {
                 </div>
                 <p className='text-[15px] font-[Poppins] text-[#388bfd] ml-6'>Api Development</p>
                 <h1 className='text-white font-[Poppins] font-[800] ml-6 text-xl'>Node.js</h1>
+            </div>
+            <div className='bg-[#343a40] w-[240px] h-[265px] rounded-[20px]'>
+                <div className='bg-[#6c757d] h-[150px] m-5 rounded-[15px] flex justify-center items-center'>
+                    <img className='w-[100px] h-[100px]' src={GIT}></img>
+                </div>
+                <p className='text-[15px] font-[Poppins] text-[#388bfd] ml-6'>Version Control</p>
+                <h1 className='text-white font-[Poppins] font-[800] ml-6 text-xl'>Git</h1>
             </div>
         </div>
     </div>
